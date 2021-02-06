000c8b553ce9a5c997b9eb73e0d30a6e4fe5ea12
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2021-02-06 14:27:18 +0100

    first draft that works

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from Library.Book import Book
+class Shop:
 
-book = Book("Wild")
-book2 = Book("bajs")
-print(book.price())
-print(book2.price())
+    def start(self):
+        stock = 0
+        while True:
+            text = input()
+            first_letter = text[0].upper()
+            if len(text) > 1:
+                amount = int(text[1:])
+                if first_letter == "S":
+                    stock = stock - amount
+                if first_letter == "I":
+                    stock = stock + amount   
+            if first_letter == "L":
+                print("Stock:", stock)
+            if first_letter == "Q":
+                break
+shop=Shop()
+shop.start()
