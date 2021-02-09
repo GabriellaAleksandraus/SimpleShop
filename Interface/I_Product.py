c1589b14c9d788a90444774b0df83abaf59d46eb
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2021-02-09 17:26:01 +0100

    Removed dead code

--- Interface/I_Product.py
+++
@@ -1,6 +0,0 @@
-class I_Product:
-
-    def name(self):
-        return "I have no name"
-    def price(self):
-        pass
--- Library/Book.py
+++
@@ -1,9 +0,0 @@
-from Interface.I_Product import I_Product
-
-class Book(I_Product):
-    def __init__(self, name):
-        self._name = name
-
-    def name(self):
-        return self._name
-
