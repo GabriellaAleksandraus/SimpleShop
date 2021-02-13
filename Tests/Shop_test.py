b94db626a47be0c4fa8e056f21533828d5a9205e
Gabriella Jakobson <gabriella_jakobson@hotmail.com>
2021-02-13 11:13:09 +0100

    Not finished

--- Tests/Shop_test.py
+++ Tests/Shop_test.py
@@ -1,6 +1,12 @@
 import unittest
 from Library.Shop import Shop
+from Library.Command_Line_Input import Command_Line_Input 
+from Library.Single_Character_Parcer import Single_Character_Parcer
+from Library.Actions import Actions
+from Library.Ram_DB import Ram_DB
 
-class TestShop_test(unittest.TestCase):
-    def test_start(self):
-        s = Shop(reader, parcer, Actions, saver)
+# To do: implement test for shop
+
+#class TestShop_test(unittest.TestCase):
+ #   def test_start(self):
+       # s = Shop(reader, parcer, Actions, saver)
